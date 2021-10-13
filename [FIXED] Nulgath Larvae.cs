bff375bae4cb60b89484d2154aaadb9ba7479424
SharpTheNightmare <36904564+SharpTheNightmare@users.noreply.github.com>
2021-10-12 21:13:53 -0500

    cleaned

--- [FIXED] Nulgath Larvae.cs
+++ [FIXED] Nulgath Larvae.cs
@@ -2,43 +2,42 @@
 
 public class Script
 {
+    public ScriptInterface bot;
 
-	public void ScriptMain(ScriptInterface bot)
-	{
-		bot.Options.SafeTimings = true;
-		bot.Options.RestPackets = true;
-		bot.Options.ExitCombatBeforeQuest = true;
+    public void ScriptMain(ScriptInterface Bot)
+    {
+        bot = Bot;
+        Bot.Options.SafeTimings = true;
+        Bot.Options.RestPackets = true;
+        Bot.Options.ExitCombatBeforeQuest = true;
 
-		bot.Skills.StartTimer();
-		bot.Skills.StartSkills("skills/Generic.xml");
+        Bot.Skills.StartTimer();
+        Bot.Skills.StartSkills("skills/Generic.xml");
 
-		ManaGolem(bot);
+        ManaGolem();
     }
 
-    public void ManaGolem(ScriptInterface bot)
-	{
-		bot.Inventory.BankAllCoinItems();
+    public void ManaGolem()
+    {
+        bot.Bank.ToInventory("Voucher of Nulgath (non-mem)");
+        bot.Bank.ToInventory("Voucher of Nulgath");
+        bot.Bank.ToInventory("Gem of Nulgath");
+        bot.Bank.ToInventory("Dark Crystal Shard");
+        bot.Bank.ToInventory("Tainted Gem");
+        bot.Bank.ToInventory("Totem of Nulgath");
+        bot.Bank.ToInventory("Diamond of Nulgath");
+        bot.Player.Join("elemental");
 
-		bot.Bank.ToInventory("Voucher of Nulgath (non-mem)");
-		bot.Bank.ToInventory("Voucher of Nulgath");
-		bot.Bank.ToInventory("Gem of Nulgath");
-		bot.Bank.ToInventory("Dark Crystal Shard");
-		bot.Bank.ToInventory("Tainted Gem");
-		bot.Bank.ToInventory("Totem of Nulgath");
-		bot.Bank.ToInventory("Diamond of Nulgath");
+        while (!bot.ShouldExit())
+        {
+            bot.Quests.EnsureAccept(2566);
 
-		bot.Player.Join("elemental");
+            bot.Player.HuntForItem("Mana Falcon", "Charged Mana Energy for Nulgath", 5, true);
+            bot.Player.HuntForItem("Mana Golem", "Mana Energy for Nulgath", 1);
 
-		while (!bot.ShouldExit())
-		{
-			bot.Quests.EnsureAccept(2566);
+            bot.Quests.EnsureComplete(2566);
 
-			bot.Player.HuntForItem("Mana Falcon", "Charged Mana Energy for Nulgath", 5, true);
-			bot.Player.HuntForItem("Mana Golem", "Mana Energy for Nulgath", 1);
-
-			bot.Quests.EnsureComplete(2566);
-
-			bot.Player.Pickup("Unidentified 13", "Voucher of Nulgath (non-mem)", "Voucher of Nulgath", "Gem of Nulgath", "Dark Crystal Shard", "Tainted Gem", "Totem of Nulgath", "Unidentified 10", "Diamond of Nulgath");
-		}
-	}
+            bot.Player.Pickup("Unidentified 13", "Voucher of Nulgath (non-mem)", "Voucher of Nulgath", "Gem of Nulgath", "Dark Crystal Shard", "Tainted Gem", "Totem of Nulgath", "Unidentified 10", "Diamond of Nulgath");
+        }
+    }
 }
