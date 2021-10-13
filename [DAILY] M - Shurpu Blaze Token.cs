cca1117757b50f1c12ccc938403feef25f13c862
SharpTheNightmare <36904564+SharpTheNightmare@users.noreply.github.com>
2021-10-12 21:44:25 -0500

    Converted and fixed 4 Daily quests

---
+++ [DAILY] M - Shurpu Blaze Token.cs
@@ -0,0 +1,56 @@
+// Converted from M - Shurpu Blaze Token
+// Author: SharpTheNightmare/purple
+// Description: daily mem sharpu blaze tokens bot
+
+using RBot;
+
+public class MemSharpuBlaze
+{
+    public string OptionsStorage = "SharpTheNightmare M - Shurpu Blaze Token";
+    public bool DontPreconfigure = true;
+
+    public int questId = 2210;
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        bot.Options.SafeTimings = true;
+        bot.Options.RestPackets = true;
+        bot.Options.SkipCutscenes = true;
+    Check:
+        if (bot.Inventory.Contains("Shurpu Blaze Token", 300))
+        {
+            goto End;
+        }
+        if (bot.Bank.Contains("Shurpu Blaze Token", 300))
+        {
+            goto End;
+        }
+        if (bot.Bank.Contains("Shurpu Blaze Token"))
+        {
+            bot.Bank.ToInventory("Shurpu Blaze Token");
+        }
+        bot.Player.Join("xancave-999999", "r11", "Left");
+        bot.Quests.EnsureAccept(questId);
+    RingGuardian:
+        bot.Player.Kill("*");
+        if (!bot.Quests.CanComplete(questId))
+        {
+            goto RingGuardian;
+        }
+        bot.Sleep(1000);
+    Quest:
+        bot.Quests.EnsureComplete(questId);
+        bot.Player.Pickup("Shurpu Blaze Token");
+    End:
+        bot.Player.Pickup("Shurpu Blaze Token");
+        if (!bot.Quests.CanComplete(questId))
+        {
+            goto Quest;
+        }
+        if (bot.Inventory.Contains("Shurpu Blaze Token"))
+        {
+            bot.Inventory.ToBank("Shurpu Blaze Token");
+        }
+        ScriptManager.StopScript();
+    }
+}
---
+++ [DAILY] NM - Glacera Ice Token.cs
@@ -0,0 +1,57 @@
+// Converted from M - Glacera Ice Token
+// Author: SharpTheNightmare/purple
+// Description: daily mem glacera ice tokens bot
+
+using RBot;
+
+public class NoMemGlaceraIce
+{
+    public string OptionsStorage = "SharpTheNightmare M - Glacera Ice Token";
+    public bool DontPreconfigure = true;
+
+    public int questId = 3966;
+
+    public void ScriptMain(ScriptInterface bot)
+    {
+        bot.Options.SafeTimings = true;
+        bot.Options.RestPackets = true;
+        bot.Options.SkipCutscenes = true;
+    Check:
+        if (bot.Inventory.Contains("Glacera Ice Token", 300))
+        {
+            goto End;
+        }
+        if (bot.Bank.Contains("Glacera Ice Token", 300))
+        {
+            goto End;
+        }
+        if (bot.Bank.Contains("Glacera Ice Token"))
+        {
+            bot.Bank.ToInventory("Glacera Ice Token");
+        }
+        bot.Player.Join("northstar-999999", "Enter", "Spawn");
+        bot.Quests.EnsureAccept(questId);
+    FrostInvader:
+        bot.Player.HuntForItem("Frost Invader", "Dark Ice", 1, true);
+        if (!bot.Quests.CanComplete(questId))
+        {
+            goto FrostInvader;
+        }
+        bot.Player.Jump("Blank", "Center");
+        bot.Sleep(1000);
+    Quest:
+        bot.Quests.EnsureComplete(questId);
+        bot.Player.Pickup("Glacera Ice Token");
+    End:
+        bot.Player.Pickup("Glacera Ice Token");
+        if (!bot.Quests.CanComplete(questId))
+        {
+            goto Quest;
+        }
+        if (bot.Inventory.Contains("Glacera Ice Token"))
+        {
+            bot.Inventory.ToBank("Glacera Ice Token");
+        }
+        ScriptManager.StopScript();
+    }
+}
