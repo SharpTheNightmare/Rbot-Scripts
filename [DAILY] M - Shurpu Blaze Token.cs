cbedb3be1695bb3a059ecb8a7024de4b9bde6c29
SharpTheNightmare <36904564+SharpTheNightmare@users.noreply.github.com>
2021-10-12 22:06:19 -0500

    Fixed type :sob:

--- [DAILY] M - Shurpu Blaze Token.cs
+++ [DAILY] M - Shurpu Blaze Token.cs
@@ -35,7 +35,7 @@ public class MemSharpuBlaze
         bot.Player.Join("xancave-999999", "r11", "Left");
         bot.Quests.EnsureAccept(questId);
     RingGuardian:
-        bot.Player.Hunt("Sharpu Ring Guardian");
+        bot.Player.Hunt("Shurpu Ring Guardian");
         if (!bot.Quests.CanComplete(questId))
         {
             goto RingGuardian;
